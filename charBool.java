13ef6482c0d3dd7acfa2af7ef871e32375dc51a2
Pravin singh <38603087+pravin1160@users.noreply.github.com>
2020-05-21 08:53:24 +0530

    Add files via upload

---
+++ charBool.java
@@ -0,0 +1,14 @@
+﻿public class CharAndBoolean {
+
+    public static void main(String[] args) {
+	    char myChar = '\u00A7';
+        System.out.println(myChar);
+
+        boolean myBoolean = true;
+        boolean isMale = true;
+
+        // Challenge
+        char registeredSymbol = '\u00AE';
+        System.out.println(registeredSymbol);
+    }
+}
